65621dffb18edb2dfe59550a72434f41b21cdd53
Zealerr <felix.rosenberg@outlook.com>
2021-02-23 17:04:23 +0100

    stuff 3

--- public/js/errormsg.js
+++ public/js/errormsg.js
@@ -1,12 +1,17 @@
-const form = document.getElementsByClassName("signupForm")[0]
+const form = document.getElementsByClassName("userForm")[0]
 const usernameInput = document.getElementsByClassName("username")[0]
 const passwordInput = document.getElementsByClassName("password")[0]
 const confirm_passwordInput = document.getElementsByClassName("confirm_pass")[0]
 
-form.addEventListener("submit", function(event) {
-  event.preventDefault()
+if (form != undefined) {
+  form.addEventListener("submit", function(event) {
+    console.log("submition startet")
+    event.preventDefault()
+    console.log("submition stopped")
+    // check if username is longer than 3 letters, has big letter
+    username = usernameInput.nodeValue()
+    console.log(username)
+  })
+}
 
-  // check if username is longer than 3 letters, has big letter
-  username = usernameInput.nodeValue()
-  console.log(username)
-})
+console.log("errormsg.js is loaded")
